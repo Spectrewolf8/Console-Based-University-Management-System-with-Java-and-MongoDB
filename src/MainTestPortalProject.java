14ec58c32015cd10c942efbdd702ca8f1637bb02
MuhammadOumar <105977684+MuhammadOumar@users.noreply.github.com>
2022-05-21 23:34:29 +0500

    Update MainTestPortalProject.java

--- src/MainTestPortalProject.java
+++ src/MainTestPortalProject.java
@@ -31,8 +31,6 @@ public class MainTestPortalProject {
 //Salman's Workspace Ending
 
 //Oumar's Workspace Starting
-    public static void TestGrades(){}
-
 //Oumar's Workspace Ending
 
 //Humayun's Workspace Starting
